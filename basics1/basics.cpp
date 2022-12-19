2d4a35764e46cd5a02a751e99839b039feda5ba2
ajay <ap221882@gmail.com>
2022-12-19 18:15:05 +0530

    feat: datatypes

---
+++ basics1/basics.cpp
@@ -0,0 +1,32 @@
+// #include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+ cout << "I'm here" << endl;
+ // Print and take outputs/inputs 
+ // int x, y;
+ // cin >> x >> y;
+ // cout << "Value of x: " << x << " and y: " << y;
+
+ // Print strings/sentences which come from input
+ // 1
+ string s1, s2;
+ cin >> s1 >> s2;
+ cout << s1 << " " << s2;
+
+ // 2
+ string str;
+ getline(cin,str);
+ cout << str;
+
+ // characters
+ char ch;
+ cin >> ch;
+ cout << ch;
+ return 0;
+}
+
+// int, long, long long
+// float, double
+// INTEGERS CAN BE STORED IN FLOATS TOO
